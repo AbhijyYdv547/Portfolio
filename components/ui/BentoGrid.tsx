dea6ecbc32913c89c4ed657ae8cf9983b22d9212
Abhijay Yadav <yadavabhijay@gmail.com>
2025-07-07 19:10:08 +0530

    Added Floating Nav

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -23,25 +23,38 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
+  id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
+  id?: number;
+  img?:string;
+  imgClassName?:string;
+  titleClassName?:string;
+  spareImg?:string;
 }) => {
   return (
     <div
       className={cn(
-        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
+        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 relative rounded-3xl border bg-white p-4 transition duration-200 hover:shadow-xl border-neutral-500 dark:border-white/[0.2] dark:bg-black dark:shadow-none ",
         className,
       )}
+
+      style={{
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
     >
-      {header}
+
       <div className="transition duration-200 group-hover/bento:translate-x-2">
-        {icon}
         <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
         </div>
