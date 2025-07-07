2d4c4c85218e3eceb2fb13e416ebdbdc31381c69
Abhijay Yadav <yadavabhijay@gmail.com>
2025-07-07 19:43:57 +0530

    Added basic bento grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { BackgroundGradientAnimation } from "./GradientBg";
 
 export const BentoGrid = ({
   className,
@@ -10,7 +11,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className,
       )}
     >
@@ -53,6 +54,42 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
+
+      <div className={`${id==6} && flex justify-center h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+            src={img}
+            alt={img}
+            className={cn(imgClassName, 'object-cover, object-center')}
+            />
+          )}
+        </div>
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className={'object-cover, object-center, w-full, h-full'}
+            />
+          )}
+        </div>
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div 
+              className="absolute z-50 flex items-center justify-center text-white-100 font-bold"
+            />
+          </BackgroundGradientAnimation>
+        )}
+
+        <div className={cn(
+          titleClassName, "group-hover/bento:translate-x-2 duration-200 relative md:h-full flex flex-col px-5 p-5 lg:p-10 min-h-40"
+          )}>
+            <div>
+              {description}
+            </div>
+        </div>
+      </div>
 
       <div className="transition duration-200 group-hover/bento:translate-x-2">
         <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
